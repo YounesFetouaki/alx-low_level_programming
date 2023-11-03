3d241cde43ada063a1c8f771717780b1e5f9a38a
YounesFetouaki <younesfetouaki65@gmail.com>
2023-11-02 17:04:54 -0700

    4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -6,7 +6,6 @@
  * @key: Hash Table Key
  * Return: Depend Condition
  */
-
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index = 0;
@@ -16,7 +15,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[index];
-	while (temp != NULL)kk
+	while (temp != NULL)
 	{
 		if (strcmp(temp->key, key) == 0)
 			return (temp->value);
